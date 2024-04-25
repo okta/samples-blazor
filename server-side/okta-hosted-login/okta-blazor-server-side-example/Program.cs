adbe2eb7ba3ca881afcd5503831f1dfddc22fc8a
Nick Drouin <nick.drouin@fox.com>
2024-04-25 14:53:20 -0400

    Removed work-around and updated to 4.6.1.

--- server-side/okta-hosted-login/okta-blazor-server-side-example/Program.cs
+++ server-side/okta-hosted-login/okta-blazor-server-side-example/Program.cs
@@ -24,41 +24,6 @@ builder.Services.AddAuthentication(options =>
         ClientId = builder.Configuration.GetValue<string>("Okta:ClientId"),
         ClientSecret = builder.Configuration.GetValue<string>("Okta:ClientSecret"),
         AuthorizationServerId = builder.Configuration.GetValue<string>("Okta:AuthorizationServerId"),
-
-        // Workaround for bug #260
-        OpenIdConnectEvents = new OpenIdConnectEvents()
-        {
-            OnTokenResponseReceived = async context =>
-            {
-                // Make our own call to Okta.AspNet.Abstractions.StrictTokenHandler
-                // First setup the parameterization as per OpenIdConnectHandler.cs:
-                var _configuration = context.Options.Configuration;
-                TokenValidationParameters validationParameters = context.Options.TokenValidationParameters.Clone();
-                validationParameters.RequireSignedTokens = false;
-                if (context.Options.ConfigurationManager is BaseConfigurationManager configurationManager)
-                {
-                    validationParameters.ConfigurationManager = configurationManager;
-                }
-                else if (_configuration != null)
-                {
-                    string[] array = new string[1] { _configuration.Issuer };
-                    validationParameters.ValidIssuers = validationParameters.ValidIssuers?.Concat(array) ?? array;
-                    validationParameters.IssuerSigningKeys = validationParameters.IssuerSigningKeys?.Concat(_configuration.SigningKeys) ?? _configuration.SigningKeys;
-                }
-                string idToken = context.TokenEndpointResponse.IdToken;
-
-                // Call the validation and check
-                TokenValidationResult tokenValidationResult = await context.Options.TokenHandler.ValidateTokenAsync(idToken, validationParameters);
-                if (tokenValidationResult.Exception != null)
-                {
-                    throw tokenValidationResult.Exception;
-                }
-
-                // Since we have already done the validation, avoid doing again with the net8.0 code
-                // which throws because of Okta's use of JwtSecurityToken instead of JsonWebToken:
-                context.Options.UseSecurityTokenValidator = true;
-            }
-        },
     });
 // To pick up AccountController:
 builder.Services.AddControllers();
